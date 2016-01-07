22b24a68c7d5a08370dc75a4e3c83357808ef330
Jose Fernando Gonzales <jfgonzal@eagle.fgcu.edu>
2016-01-07 00:45:01 -0500

    installed gulp and gutil

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/**
+ * Created by zera on 1/7/2016.
+ */
+var gulp = require('gulp'),
+    gutil = require('gulp-util');
+
+gulp.task('log', function(){
+    gutil.log('Workflows are working');
+});
