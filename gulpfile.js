c502025f92d62daf8a553d38f63c1c26fd895b35
Jose Fernando Gonzales <jfgonzal@eagle.fgcu.edu>
2016-01-09 21:50:49 -0500

    Crushed JSON file

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var gulp = require('gulp'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
     minifyHTML = require('gulp-minify-html'),
+    jsonminify = require('gulp-jsonminify'),
     concat = require('gulp-concat');
 
 var env,
@@ -77,7 +78,7 @@ gulp.task('watch', function(){
    gulp.watch(jsSources, ['js']);
    gulp.watch('components/sass/*.scss', ['compass']);
    gulp.watch('builds/development/*.html', ['html']);
-   gulp.watch(jsonSources, ['json']);
+   gulp.watch('builds/development/js/*.json', ['json']);
 });
 
 gulp.task('connect', function(){
@@ -95,7 +96,9 @@ gulp.task('html', function(){
 });
 
 gulp.task('json', function(){
-    gulp.src(jsonSources)
+    gulp.src('builds/development/js/*.json')
+        .pipe(gulpif(env != 'development', jsonminify()))
+        .pipe(gulpif(env != 'development', gulp.dest('builds/production/js')))
         .pipe(connect.reload())
 });
 
